600eb6ba7c1effe36101de12754367867827f610
Davi Freire <fsdavi.dev@gmail.com>
2021-06-16 21:08:49 -0300

    Creates view user-identification and start stylize

---
+++ src/views/user-identification.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="page">
+    <pokedex-logo />
+    <box-dialog :messageText="'Whats your name, trainer?'" />
+    <div class="inputName-container">
+      <input type="text" class="inputName" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+import PokedexLogo from '../components/BaseLogo.vue';
+import BoxDialog from '../components/BoxDialog.vue';
+
+@Component({
+  components: {
+    'pokedex-logo': PokedexLogo,
+    'box-dialog': BoxDialog,
+  },
+})
+export default class UserIdentification extends Vue {}
+</script>
+
+<style scoped src="../styles/views/user-identification.css">
+</style>
