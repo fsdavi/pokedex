451f0c21b4ae59e58686ecd6e7c237013b8d59ce
Davi Freire <fsdavi.dev@gmail.com>
2021-06-16 21:06:21 -0300

    Change file name, some styles and creates all logic to select a trainer and redirect to the next view

---
+++ src/views/trainer-selection.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="page">
+    <pokedex-logo />
+    <div class="container-trainer-selection">
+      <button
+        class="container-trainer bg-button-blue"
+        @click="handleClickTrainer('brendan')"
+        :class="trainer === 'brendan' ? 'selected-trainer' : ''"
+      >
+        <img
+          src="../assets/brendan.svg"
+          alt="Trainer"
+          class="imgBrendan-trainer"
+        />
+      </button>
+      <button
+        class="container-trainer bg-button-red"
+        @click="handleClickTrainer('may')"
+        :class="trainer === 'may' ? 'selected-trainer' : ''"
+      >
+        <img src="../assets/may.svg" alt="Trainer" class="imgMay-trainer" />
+      </button>
+    </div>
+    <box-dialog :messageText="'Choose your trainer!'" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+import { mapGetters, mapActions } from 'vuex';
+
+import PokedexLogo from '../components/BaseLogo.vue';
+import BoxDialog from '../components/BoxDialog.vue';
+
+@Component({
+  components: {
+    'pokedex-logo': PokedexLogo,
+    'box-dialog': BoxDialog,
+  },
+  computed: {
+    ...mapGetters({ trainer: 'trainer' }),
+  },
+  methods: {
+    ...mapActions({ selectTrainer: 'selectTrainer' }),
+  },
+})
+export default class TrainerSelection extends Vue {
+  public selectTrainer!: (trainer: string) => void;
+
+  handleClickTrainer(trainer: string): void {
+    this.selectTrainer(trainer);
+
+    setTimeout(() => {
+      this.$router.push({ name: 'Identification' });
+    }, 2000);
+  }
+}
+</script>
+
+<style scoped src="../styles/views/trainer-selection.css">
+</style>
