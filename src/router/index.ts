2b7238f712c3490508e34ee848a4c57a7482677c
Davi Freire <fsdavi.dev@gmail.com>
2021-06-16 21:09:07 -0300

    Adjust the router file to the identification view

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,20 @@
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
-import Home from '../views/Home.vue';
+import TrainerSelection from '../views/trainer-selection.vue';
+import UserIdentification from '../views/user-identification.vue';
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
     path: '/',
-    name: 'Home',
-    component: Home,
+    name: 'TrainerSelection',
+    component: TrainerSelection,
+  }, {
+    path: '/identification',
+    name: 'Identification',
+    component: UserIdentification,
   },
-  // {
-  //   path: '/pokedex',
-  //   name: 'Pokedéx',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  // },
 ];
 
 const router = new VueRouter({
