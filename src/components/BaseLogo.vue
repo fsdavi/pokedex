4a00eedc35c5711547e1e4f8a4d92d2b53c1267e
Davi Freire <fsdavi.dev@gmail.com>
2021-06-16 21:03:54 -0300

    Creates a BaseLogo component removing from Home to be avaible for others components

---
+++ src/components/BaseLogo.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container-logo">
+    <img class="pokedex-logo" src="../assets/pokedex-logo.png" alt="Logo" />
+    <img class="pokeball" src="../assets/pokeball.svg" alt="Pokeball" />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped src="../styles/components/PokeLogo.css">
+</style>
